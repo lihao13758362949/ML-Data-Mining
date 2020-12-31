b70b615ff0b9dc6bc2d5a31900296731f19c2e58
lihao <41355487+lihao13758362949@users.noreply.github.com>
2020-12-31 14:55:58 +0000

    支持向量机

--- model/SVC.py
+++ model/SVC.py
@@ -1,6 +1,10 @@
 from sklearn.svm import SVC
 
-model = SVC()
+class_weights = {0:1, 1:5}#权重
+kernels = ["rbf","poly","sigmoid","linear"]
+c_list = [0.01, 0.1, 1, 10, 100]
+
+model = SVC(class_weight = class_weights,kernel = kernels[0],C=C[0])
 model.fit(X_train, y_train)
 
 pred = model.predict(X_test)
