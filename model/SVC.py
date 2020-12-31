e3a5b5136dba098bef62f70cbf39fcaf9330228a
lihao <41355487+lihao13758362949@users.noreply.github.com>
2020-12-31 14:46:17 +0000

    支持向量机

--- model/SVC.py
+++ model/SVC.py
@@ -1,10 +1,6 @@
-from sklearn import metrics
 from sklearn.svm import SVC
+
 model = SVC()
 model.fit(X_train, y_train)
 
 pred = model.predict(X_test)
-
-print (metrics.classification_report(y_test, pred))
-print (metrics.confusion_matrix(y_test, pred))
-print (metrics.accuracy_score(y_test, pred))
