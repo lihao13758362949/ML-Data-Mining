0fffd9c80343f70f3d6d960fcb2ed9846ad224f9
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-01-01 02:27:28 +0000

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -1,16 +1,13 @@
-# 导入库
+# 数据探索性分析
+
+# 1.导入库
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# 设置
-import warnings
-warnings.filterwarnings("ignore")
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.rcParams['axes.unicode_minus']=False
 
-# 读取数据
+# 2.读取数据
 df=pd.read_excel(r'C:\Users\lihao\Desktop\学年论文\链家二手房房源信息精简.xlsx',header=0,names=['price','layout','floor','direction','fitup','area','type','region','look_7','look_30'])
 # 连接数据
 df = pd.concat([train.assign(is_train=1), test.assign(is_train=0)])
@@ -47,19 +44,30 @@ def get_data(path, get_type=True):
 df_train = get_data(trn_path)
 df_test = get_data(test_path, False)
 
-# 数据集基本信息（当前因变量为SalePrice）
+# 3.数据集基本信息（当前因变量为SalePrice）
 df.info()
-df.columns #：显示所有的变量名
+
+columns = df.columns.values.tolist() #获取所有的变量名
+
 df.shape #：shape
+
 df.head() #：给前几个样本
 df.tail() #：给后几个样本
 df.sample(10) #：随机给几个样本
+
 df.describe() #：连续变量的一些描述信息，如基本统计量、分布等。
 df.describe(include=['O']) #：分类变量的一些描述信息。
 df.describe(include='all') #：全部变量的一些描述信息。
+
 df.SalePrice.value_counts() #：观察取值数量
+
 # df.SalePrice.value_counts(1) #：观察取值比例
+
 credit.isnull().sum()/float(len(credit)) # 缺失值
+
+df_value_ravel = df.values.ravel() 
+print (u'缺失值数量：',len(df_value_ravel[df_value_ravel==np.nan])) # 缺失值
+
 
 # 作图
 sns.distplot(df['price'])
