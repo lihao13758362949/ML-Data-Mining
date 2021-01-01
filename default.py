dd1b2718a481fa3a5890f5acf007038c3ea747b2
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-01-01 02:14:29 +0000

    初始设置

--- default.py
+++ default.py
@@ -1,3 +1,12 @@
+# 初始设置
+
 import pandas as pd
 
-pd.set_option("display.max_columns",32)
+pd.set_option("display.max_columns",32) # 显示最大列数
+
+import warnings
+warnings.filterwarnings("ignore") # 过滤警告文字
+
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False # 在图片中显示中文
