462c5264d473783f886bca2df9baf922d8998868
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-01-02 07:06:01 +0000

    Update default.py

--- default.py
+++ default.py
@@ -3,9 +3,13 @@
 import pandas as pd
 
 pd.set_option("display.max_columns",32) # 显示最大列数
-
+pd.options.display.max_rows = 1000
+pd.options.display.max_columns = 20
 import warnings
 warnings.filterwarnings("ignore") # 过滤警告文字
+
+# plt设置
+%matplotlib inline # 不用show就可以显示图片
 
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif']=['SimHei']
