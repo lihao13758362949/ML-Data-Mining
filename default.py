70789a26f1bfe84475dacc33ec9396a5ad5f0078
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-01-02 08:54:39 +0000

    Update default.py

--- default.py
+++ default.py
@@ -1,10 +1,15 @@
 # 初始设置
 
+
+#pandas设置
 import pandas as pd
 
-pd.set_option("display.max_columns",32) # 显示最大列数
-pd.options.display.max_rows = 1000
-pd.options.display.max_columns = 20
+#pd.set_option("display.max_columns",32) # 显示最大列数
+pd.options.display.max_rows = 1000 # 显示最大行数
+pd.options.display.max_columns = 20 # 显示最大列数
+pd.set_option('display.float_format', lambda x: '%.3f' % x) # 小数格式设置
+
+# warning设置
 import warnings
 warnings.filterwarnings("ignore") # 过滤警告文字
 
