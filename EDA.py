0047803d5d726e5071ffda3106b3c8a17d3ed86d
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-01-05 02:05:20 +0000

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -8,20 +8,30 @@ import seaborn as sns
 import scipy.stats as st
 
 # 2.读取数据
-train = pd.read_csv('../input/train.csv')
-df=pd.read_excel(r'C:\Users\lihao\Desktop\学年论文\链家二手房房源信息精简.xlsx',header=0,names=['price','layout','floor','direction','fitup','area','type','region','look_7','look_30'])
+import os
+import pandas as pd
+
+# train_path=''
+# test_path=''
+def get_data(path,data_type='csv',header=None):
+    ## 注意path格式，可能要加r
+    if data_type == 'csv':
+        df = pd.read_csv(path,header=header)
+    if data_type=='xlsx'
+        df=pd.read_excel(path,header=header,names=['price','layout','floor','direction','fitup','area','type','region','look_7','look_30'])
+    if data_type='arff'
+        # 读取arff文件
+        from scipy.io import arff
+        data,meta=arff.loadarff(path)
+        df=pd.DataFrame(data)
+
+    return df
+df_train = get_data(trn_path)
+df_test = get_data(test_path)
 # 连接数据
-df = pd.concat([train.assign(is_train=1), test.assign(is_train=0)])
-
-# 读取arff文件
-from scipy.io import arff
-import pandas as pd 
-file_name='1year.arff'
-data,meta=arff.loadarff(file_name)
-df=pd.DataFrame(data)
-
+all_data = pd.concat([df_train.assign(is_train=1), df_test.assign(is_train=0)])
 # 多文件读取
-def get_data(path, get_type=True):
+def get_data2(path, get_type=True):
     features = []
     for file in tqdm(os.listdir(path)):
         file_path = os.path.join(path, file)
@@ -47,12 +57,6 @@ def get_data(path, get_type=True):
         df['label'] = df['label'].map(label_dict)
     else:
         df = df.rename(columns={len(features[0])-1:'filename'})
-    
-
-    return df
-df_train = get_data(trn_path)
-df_test = get_data(test_path, False)
-
 # 3.数据集基本信息（当前因变量为SalePrice）
 df.info()
 
@@ -131,38 +135,47 @@ print("Kurtosis: %f" % df_train['SalePrice'].kurt())
 
 
 # 6. 相关变量探索
-# 散点图（数字变量）
-data = pd.concat([df['price'], df['area']], axis=1)
-data.plot.scatter(x='area', y='price', ylim=(0,40000));
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+def plot(X,y,X_cols,y_col,plot_type=scatter):
+    #scatter:散点图，pairplot:sns.pairplot，box：箱图，hist：直方图，heatmap：相关分析，热度图，list:列表汇总
+    if plot_type==scatter:
+        # 散点图（数字变量）
+        data = pd.concat([X, y], axis=1)
+        data.plot.scatter(x=X_cols, y=y_col);
+    if plot_type==pairplot:
+        #sns.pairplot
+        sns.set()
+        data = pd.concat([X, y], axis=1)
+        sns.pairplot(data,x_vars=X_cols,y_vars=y_col)#参数size=2.5表示大小，aspect=0.8表示，kind='reg'添加拟合直线和95%置信区间
+        plt.show();
+    if plot_type=box:
+        # 箱图（分类变量）
+        var = 'region'
+        data = pd.concat([df[y_col], df[var]], axis=1)
+        f, ax = plt.subplots(figsize=(8, 6))
+        fig = sns.boxplot(x=var, y=y_col, data=data)
+        # fig.axis(ymin=0, ymax=800000);
+    if plot_type=hist:
+        # 对比，直方图
+        g = sns.FacetGrid(train_df, col='Survived')
+        g.map(plt.hist, 'Age', bins=20)
+    if plot_type=heatmap:
+        # 相关分析，热度图heatmaps1
+        corrmat = df_train.corr()
+        f, ax = plt.subplots(figsize=(12, 9))
+        sns.heatmap(corrmat, vmax=.8, square=True);
+        # 选出和目标变量最相关的k个变量
+        k = 10 #number of variables for heatmap
+        cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+        cm = np.corrcoef(df_train[cols].values.T)
+        sns.set(font_scale=1.25)
+        hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+        plt.show()
+    if plot_type=list:
+        # 列表汇总（分类变量+数字变量）
+        train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
 
-#pairplot
-sns.set()
-cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
-sns.pairplot(df_train[cols], size = 2.5)
-plt.show();
 
-# 箱图（分类变量）
-var = 'region'
-data = pd.concat([df['price'], df[var]], axis=1)
-f, ax = plt.subplots(figsize=(8, 6))
-fig = sns.boxplot(x=var, y="price", data=data)
-# fig.axis(ymin=0, ymax=800000);
 
-# 对比，直方图
-g = sns.FacetGrid(train_df, col='Survived')
-g.map(plt.hist, 'Age', bins=20)
-
-# 列表汇总（分类变量+数字变量）
-train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
-
-# 相关分析，热度图heatmaps1
-corrmat = df_train.corr()
-f, ax = plt.subplots(figsize=(12, 9))
-sns.heatmap(corrmat, vmax=.8, square=True);
-# 选出和目标变量最相关的k个变量
-k = 10 #number of variables for heatmap
-cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
-cm = np.corrcoef(df_train[cols].values.T)
-sns.set(font_scale=1.25)
-hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
-plt.show()
