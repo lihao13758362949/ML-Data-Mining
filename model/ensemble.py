aa3734f01d8b9cb1f385683b2add26e52e54bad0
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-01-30 17:51:16 +0800

    Update ensemble.py

--- model/ensemble.py
+++ model/ensemble.py
@@ -1,5 +1,4 @@
-# 集成学习
-
+# 集成学习 输入：splits,metrics,X_test
 
 from sklearn import datasets  
 from sklearn import model_selection  
@@ -8,6 +7,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import VotingClassifier
 import numpy as np
+
+
 
 # Voting
 # 简单投票法
@@ -30,7 +31,11 @@ for clf, label in zip([clf1, clf2, clf3, sclf],
   
     scores = model_selection.cross_val_score(clf, x_data, y_data, cv=3, scoring='accuracy')  
     print("Accuracy: %0.2f [%s]" % (scores.mean(), label)) 
-    
+   
+  
+  
+  
+  
 # 简单的平均
 class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
     def __init__(self, models):
@@ -52,6 +57,10 @@ class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
             model.predict(X) for model in self.models_
         ])
         return np.mean(predictions, axis=1)
+        
+        
+        
+        
         
 # 用基学习器的预测结果作为输入训练二级学习器
 class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
@@ -88,6 +97,10 @@ class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
             np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
             for base_models in self.base_models_ ])
         return self.meta_model_.predict(meta_features)
+      
+      
+      
+      
 from sklearn import linear_model
 # 将lgb和xgb和ctb的结果进行stacking
 train_stack = np.vstack([oof_lgb,oof_xgb,oof_cb]).transpose()
@@ -112,6 +125,10 @@ for fold_, (trn_idx, val_idx) in enumerate(folds_stack.split(train_stack,y_train
     
 print("CV score: {:<8.8f}".format(mean_squared_error(oof_stack, y_train_)))
     
+  
+  
+  
+  
 # Stacking
 # 先训练初级学习器，然后用预测值来训练次级学习器
 from sklearn import datasets
@@ -143,6 +160,62 @@ for clf, label in zip([clf1, clf2, clf3, sclf],
     print("Accuracy: %0.2f [%s]" % (scores.mean(), label))
 
     
+# Boosting类
+## LightGBM
+import numpy as np
+import pandas as pd
+import lightgbm as lgb
+
+lgb_param = {
+    'task' : 'train',
+    'boosting_type' : 'gbdt',
+    'objective' : 'binary',
+    'metric' : {'binary_error'},
+    'num_leaves' : 120,
+    'learning_rate' : 0.02,
+    'feature_fraction' : 0.7,
+    'bagging_fraction' : 0.7,
+    'bagging_freq' : 5,
+    # 'min_data_in_leaf' : 10,
+    'verbose' : 0
+}
+
+cv_score = []
+res = np.zeros(X_test.shape[0])
+
+for idx,(train_idx,val_idx) in enumerate(splits):
+    print('-'*50)
+    print('iter{}'.format(idx+1))
+    X_trn,y_trn = X_train[train_idx],y_train[train_idx]
+    X_val,y_val = X_train[val_idx],y_train[val_idx]
+
+    dtrn = lgb.Dataset(X_trn,label=y_trn)
+    dval = lgb.Dataset(X_val, label=y_val)
+
+    bst = lgb.train(lgb_param,dtrn,500000,valid_sets=dval,
+                    early_stopping_rounds=150,verbose_eval=50)
+    preds = bst.predict(X_val,num_iteration=bst.best_iteration)
+    lgb_pred[val_idx] = preds
+
+    preds = transform(preds)
+
+    score = accuracy_score(y_val,preds)
+    print(score)
+    cv_score.append(score)
+    res += transform(bst.predict(X_test,num_iteration=bst.best_iteration)) # 输出时要改为0或1
+
+    dfFeature = pd.DataFrame()
+    dfFeature['featureName'] = train[feature].columns
+    dfFeature['score'] = bst.feature_importance()
+    dfFeature = dfFeature.sort_values(by='score',ascending=False)
+    dfFeature.to_csv('featureImportance.csv',index=False,sep='\t')
+    print(dfFeature)
+    
+print('offline mean ACC score:',np.mean(cv_score))
+    
+    
+    
+    
 1. `.AdaBoostRegressor(base_estimator=None, n_estimators=50, learning_rate=1.0, loss=’linear’, random_state=None)`
 >`n_estimators`：基学习器数量
 >`base_estimator`：基学习器类型，默认`.tree.DecisionTreeRegressor(max_depth=3)`
@@ -157,9 +230,14 @@ for clf, label in zip([clf1, clf2, clf3, sclf],
 13. `.HistGradientBoostingClassifier(loss=’auto’, learning_rate=0.1, max_iter=100, max_leaf_nodes=31, max_depth=None, min_samples_leaf=20, l2_regularization=0.0, max_bins=256, scoring=None, validation_fraction=0.1, n_iter_no_change=None, tol=1e-07, verbose=0, random_state=None)`：数据量较大时效果比`GradientBoostingClassifier`好得多。
 14. `.HistGradientBoostingRegressor(loss=’least_squares’, learning_rate=0.1, max_iter=100, max_leaf_nodes=31, max_depth=None, min_samples_leaf=20, l2_regularization=0.0, max_bins=256, scoring=None, validation_fraction=0.1, n_iter_no_change=None, tol=1e-07, verbose=0, random_state=None)`
 
-# RF随机森林
-## 随机森林的优点：防过拟合、抗噪声、无需规范化、速度快、
-## 随机森林的缺点：某些噪声过大的问题上会过拟合、对取值多的变量有偏好，因此属性权值不可信
+
+
+
+# Bagging类
+
+## RF随机森林
+### 随机森林的优点：防过拟合、抗噪声、无需规范化、速度快、
+### 随机森林的缺点：某些噪声过大的问题上会过拟合、对取值多的变量有偏好，因此属性权值不可信
 # `.RandomForestClassifier(n_estimators=’warn’, criterion=’gini’, max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=’auto’, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=False, n_jobs=None, random_state=None, verbose=0, warm_start=False, class_weight=None)`
 # `.RandomForestRegressor(n_estimators=’warn’, criterion=’mse’, max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=’auto’, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=False, n_jobs=None, random_state=None, verbose=0, warm_start=False)`
 from sklearn.ensemble import RandomForestClassifier
@@ -172,7 +250,7 @@ std = np.std([tree.feature_importances_ for tree in clf.estimators_],axis=0)
 #按照变量的重要性排序后的索引 
 indices = np.argsort(importances)[::-1]
 
-# 绘图过程
+### 绘图过程
 import matplotlib.pyplot as plt
 plt.figure()
 plt.figure(figsize=(10,5))
