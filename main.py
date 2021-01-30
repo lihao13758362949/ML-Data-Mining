fa916d21a67341cc97fb37a3f408f73a2a977408
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-01-30 17:52:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-# 1 EDA
+# 1 EDA 输出train,test
 
-# 2 数据预处理
+# 2 数据预处理 输入：train,test 输出：train,test
 
-# 3. model前准备
-## model_selection
-## metrics
+# 3. model前准备 输入：train,test
+feature = [x for x in train.columns if x not in ['187']]
+
+y_train = train['187']
+X_train = train[feature].values
+X_test = test[feature].values
+## model_selection 输入：训练集X和y 输出：splits
+## metrics 输出：metrics
 
 
-# 4. model
+# 4. model #输入：splits,metrics,X_test
+## model_params
