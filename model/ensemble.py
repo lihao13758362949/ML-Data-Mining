2e14b50be73d967c1e9b6f18bd7d56b878addb51
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-02-09 10:35:48 +0800

    Update ensemble.py

--- model/ensemble.py
+++ model/ensemble.py
@@ -1,5 +1,9 @@
-# 集成学习 输入：splits,metrics,X_test 输出：score（cv_score或）,res,模型（feature_importance，best_iteration）
+'''
+集成学习 ensemble.py
+输入：splits,metrics,X_test
+输出：score（cv_score或）,res,模型（feature_importance，best_iteration）
 
+'''
 from sklearn import datasets  
 from sklearn import model_selection  
 from sklearn.linear_model import LogisticRegression
@@ -10,8 +14,7 @@ import numpy as np
 
 
 
-# Voting
-# 简单投票法
+# <Voting> 简单投票法
 # 载入数据集
 iris = datasets.load_iris()  
 # 只要第1,2列的特征
@@ -36,7 +39,7 @@ for clf, label in zip([clf1, clf2, clf3, sclf],
   
   
   
-# 简单的平均
+# <简单的平均>
 class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
     def __init__(self, models):
         self.models = models
@@ -62,7 +65,7 @@ class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
         
         
         
-# 用基学习器的预测结果作为输入训练二级学习器
+# <用基学习器的预测结果作为输入训练二级学习器>
 class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
     def __init__(self, base_models, meta_model, n_folds=5):
         self.base_models = base_models
@@ -99,69 +102,32 @@ class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
         return self.meta_model_.predict(meta_features)
       
       
-      
-      
-from sklearn import linear_model
-# 将lgb和xgb和ctb的结果进行stacking
-train_stack = np.vstack([oof_lgb,oof_xgb,oof_cb]).transpose()
-test_stack = np.vstack([predictions_lgb, predictions_xgb,predictions_cb]).transpose()
+# 1 <Boosting>
+# >能够降低模型的bias，迭代地训练 Base Model，每次根据上一个迭代中预测错误的情况修改训练样本的权重。也即 Gradient Boosting 的原理。比 Bagging 效果好，但更容易 Overfit。
+## 1.1 <AdaBoost>
+# >提高前一轮弱分类器错误分类样本的权值，降低被正确分类样本的权值。
+# >加权投票法组合分类器
+AdaBoostRegressor(base_estimator=None, n_estimators=50, learning_rate=1.0, loss=’linear’, random_state=None)
+AdaBoostClassifier(base_estimator=None, n_estimators=50, learning_rate=1.0, algorithm=’SAMME.R’, random_state=None)
+'''
+n_estimators：基学习器数量
+base_estimator：基学习器类型，默认`.tree.DecisionTreeRegressor(max_depth=3)`
+'''
+
+## 1.2 <boosting tree>（提升树）
+# >提升树算法（向前分布算法，逐渐减少残差） 
+# >注：提升树算法仅在损失函数为平方误差损失函数时适用
 
 
-folds_stack = RepeatedKFold(n_splits=5, n_repeats=2, random_state=2018)
-oof_stack = np.zeros(train_stack.shape[0])
-predictions = np.zeros(test_stack.shape[0])
 
-for fold_, (trn_idx, val_idx) in enumerate(folds_stack.split(train_stack,y_train)):
-    print("fold {}".format(fold_))
-    trn_data, trn_y = train_stack[trn_idx], y_train[trn_idx]
-    val_data, val_y = train_stack[val_idx], y_train[val_idx]
-    
-    clf_3 = linear_model.BayesianRidge()
-    #clf_3 =linear_model.Ridge()
-    clf_3.fit(trn_data, trn_y)
-    
-    oof_stack[val_idx] = clf_3.predict(val_data)
-    predictions += clf_3.predict(test_stack) / 10
-    
-print("CV score: {:<8.8f}".format(mean_squared_error(oof_stack, y_train_)))
-    
-  
-  
-  
-  
-# Stacking
-# 先训练初级学习器，然后用预测值来训练次级学习器
-from sklearn import datasets
-iris = datasets.load_iris()
-X, y = iris.data[:, 1:3], iris.target
+## 1.3 <Gradient Boosting Decision Tree> （GBDT，梯度提升树）
+# >一般化的提升树算法
+GradientBoostingClassifier(loss=’deviance’, learning_rate=0.1, n_estimators=100, subsample=1.0, criterion=’friedman_mse’, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0, min_impurity_split=None, init=None, random_state=None, max_features=None, verbose=0, max_leaf_nodes=None, warm_start=False, presort=’auto’, validation_fraction=0.1, n_iter_no_change=None, tol=0.0001)
+GradientBoostingRegressor(loss=’ls’, learning_rate=0.1, n_estimators=100, subsample=1.0, criterion=’friedman_mse’, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0, min_impurity_split=None, init=None, random_state=None, max_features=None, alpha=0.9, verbose=0, max_leaf_nodes=None, warm_start=False, presort=’auto’, validation_fraction=0.1, n_iter_no_change=None, tol=0.0001)
 
-from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB 
-from sklearn.ensemble import RandomForestClassifier
-from mlxtend.classifier import StackingClassifier
-import numpy as np
+## 1.4 <XGBoost>
 
-clf1 = KNeighborsClassifier(n_neighbors=1)
-clf2 = RandomForestClassifier(random_state=1)
-clf3 = GaussianNB()
-lr = LogisticRegression()
-sclf = StackingClassifier(classifiers=[clf1, clf2, clf3], 
-                          meta_classifier=lr)
-      
-for clf, label in zip([clf1, clf2, clf3, sclf], 
-                      ['KNN', 
-                       'Random Forest', 
-                       'Naive Bayes',
-                       'StackingClassifier']):
- 
-    scores = model_selection.cross_val_score(clf, X, y,cv=3, scoring='accuracy')
-    print("Accuracy: %0.2f [%s]" % (scores.mean(), label))
-
-    
-# Boosting类
-## LightGBM
+## 1.5 <LightGBM>
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
@@ -216,24 +182,20 @@ print('offline mean ACC score:',np.mean(cv_score))
     
     
     
-1. `.AdaBoostRegressor(base_estimator=None, n_estimators=50, learning_rate=1.0, loss=’linear’, random_state=None)`
->`n_estimators`：基学习器数量
->`base_estimator`：基学习器类型，默认`.tree.DecisionTreeRegressor(max_depth=3)`
-2. `.AdaBoostClassifier(base_estimator=None, n_estimators=50, learning_rate=1.0, algorithm=’SAMME.R’, random_state=None)`
+
 3. `.BaggingClassifier(base_estimator=None, n_estimators=10, max_samples=1.0, max_features=1.0, bootstrap=True, bootstrap_features=False, oob_score=False, warm_start=False, n_jobs=None, random_state=None, verbose=0)`
 4. `.BaggingRegressor(base_estimator=None, n_estimators=10, max_samples=1.0, max_features=1.0, bootstrap=True, bootstrap_features=False, oob_score=False, warm_start=False, n_jobs=None, random_state=None, verbose=0)`
 
 7. `.ExtraTreesClassifier(n_estimators=’warn’, criterion=’gini’, max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=’auto’, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=False, oob_score=False, n_jobs=None, random_state=None, verbose=0, warm_start=False, class_weight=None)`
 8. `.ExtraTreesRegressor(n_estimators=’warn’, criterion=’mse’, max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=’auto’, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=False, oob_score=False, n_jobs=None, random_state=None, verbose=0, warm_start=False)`
-11. `.GradientBoostingClassifier(loss=’deviance’, learning_rate=0.1, n_estimators=100, subsample=1.0, criterion=’friedman_mse’, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0, min_impurity_split=None, init=None, random_state=None, max_features=None, verbose=0, max_leaf_nodes=None, warm_start=False, presort=’auto’, validation_fraction=0.1, n_iter_no_change=None, tol=0.0001)`
-12. `.GradientBoostingRegressor(loss=’ls’, learning_rate=0.1, n_estimators=100, subsample=1.0, criterion=’friedman_mse’, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, min_impurity_decrease=0.0, min_impurity_split=None, init=None, random_state=None, max_features=None, alpha=0.9, verbose=0, max_leaf_nodes=None, warm_start=False, presort=’auto’, validation_fraction=0.1, n_iter_no_change=None, tol=0.0001)`
+
 13. `.HistGradientBoostingClassifier(loss=’auto’, learning_rate=0.1, max_iter=100, max_leaf_nodes=31, max_depth=None, min_samples_leaf=20, l2_regularization=0.0, max_bins=256, scoring=None, validation_fraction=0.1, n_iter_no_change=None, tol=1e-07, verbose=0, random_state=None)`：数据量较大时效果比`GradientBoostingClassifier`好得多。
 14. `.HistGradientBoostingRegressor(loss=’least_squares’, learning_rate=0.1, max_iter=100, max_leaf_nodes=31, max_depth=None, min_samples_leaf=20, l2_regularization=0.0, max_bins=256, scoring=None, validation_fraction=0.1, n_iter_no_change=None, tol=1e-07, verbose=0, random_state=None)`
 
 
 
 
-# Bagging类
+# 2 <Bagging>
 
 ## RF随机森林
 ### 随机森林的优点：防过拟合、抗噪声、无需规范化、速度快、
@@ -261,3 +223,69 @@ plt.xticks(range(X.shape[1]), df.columns.values[:-1][indices],rotation=40)
 plt.xlim([-1, X.shape[1]])
 plt.tight_layout()
 plt.show()
+     
+      
+
+    
+  
+  
+  
+  
+# 3 <Stacking>
+# 先训练初级学习器，然后用预测值来训练次级学习器
+from sklearn import datasets
+iris = datasets.load_iris()
+X, y = iris.data[:, 1:3], iris.target
+
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB 
+from sklearn.ensemble import RandomForestClassifier
+from mlxtend.classifier import StackingClassifier
+import numpy as np
+
+clf1 = KNeighborsClassifier(n_neighbors=1)
+clf2 = RandomForestClassifier(random_state=1)
+clf3 = GaussianNB()
+lr = LogisticRegression()
+sclf = StackingClassifier(classifiers=[clf1, clf2, clf3], 
+                          meta_classifier=lr)
+      
+for clf, label in zip([clf1, clf2, clf3, sclf], 
+                      ['KNN', 
+                       'Random Forest', 
+                       'Naive Bayes',
+                       'StackingClassifier']):
+ 
+    scores = model_selection.cross_val_score(clf, X, y,cv=3, scoring='accuracy')
+    print("Accuracy: %0.2f [%s]" % (scores.mean(), label))
+
+    
+from sklearn import linear_model
+# 将lgb和xgb和ctb的结果进行stacking
+train_stack = np.vstack([oof_lgb,oof_xgb,oof_cb]).transpose()
+test_stack = np.vstack([predictions_lgb, predictions_xgb,predictions_cb]).transpose()
+
+
+folds_stack = RepeatedKFold(n_splits=5, n_repeats=2, random_state=2018)
+oof_stack = np.zeros(train_stack.shape[0])
+predictions = np.zeros(test_stack.shape[0])
+
+for fold_, (trn_idx, val_idx) in enumerate(folds_stack.split(train_stack,y_train)):
+    print("fold {}".format(fold_))
+    trn_data, trn_y = train_stack[trn_idx], y_train[trn_idx]
+    val_data, val_y = train_stack[val_idx], y_train[val_idx]
+    
+    clf_3 = linear_model.BayesianRidge()
+    #clf_3 =linear_model.Ridge()
+    clf_3.fit(trn_data, trn_y)
+    
+    oof_stack[val_idx] = clf_3.predict(val_data)
+    predictions += clf_3.predict(test_stack) / 10
+    
+print("CV score: {:<8.8f}".format(mean_squared_error(oof_stack, y_train_)))    
+    
+    
+    
+    
