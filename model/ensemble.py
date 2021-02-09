2ffc21a88bc19eb9cbb8c9604ebac274067d9348
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-02-09 10:42:16 +0800

    Update ensemble.py

--- model/ensemble.py
+++ model/ensemble.py
@@ -196,10 +196,29 @@ print('offline mean ACC score:',np.mean(cv_score))
 
 
 # 2 <Bagging>
+# >独立的训练一些基学习器(一般倾向于强大而复杂的模型比如完全生长的决策树)，然后综合他们的预测结果。
+"""
+通常为了获得差异性较大的基学习器，我们对不同的基学习器给不同的训练数据集。根据**采样方式**有以下变体：
+Pasting:直接从样本集里随机抽取的到训练样本子集
+Bagging:自助采样(有放回的抽样)得到训练子集
+Random Subspaces:列采样,按照特征进行样本子集的切分
+Random Patches:同时进行行采样、列采样得到样本子集
 
-## RF随机森林
-### 随机森林的优点：防过拟合、抗噪声、无需规范化、速度快、
-### 随机森林的缺点：某些噪声过大的问题上会过拟合、对取值多的变量有偏好，因此属性权值不可信
+当训练了许多基学习器后，将他们加权平均（连续）或投票法（离散）得到最终学习器。
+
+这里给出投票法的几种类型：
+绝对多数投票法：如果标记投票超过半数则预测标记，否则拒绝预测。
+相对多数投票法：预测为得票最多的标记，若有多个得票相同，则随机选取一个。
+加权投票法：以学习器的准确率为权重加权投票，并选择最多的票数标记。
+"""
+## 2.1 <Random Forest 随机森林>
+# >随机森林的优点：防过拟合、抗噪声、无需规范化、速度快、
+# >随机森林的缺点：某些噪声过大的问题上会过拟合、对取值多的变量有偏好，因此属性权值不可信
+# >随机森林在基学习器较少的时候表现不太好，但随着基学习器数目的增加，随机森林通常会收敛到更低的方差。
+# >和决策树算法类似，先从候选划分属性中随机选取$k=log_2d$（推荐）个属性，接着用划分算法选择最优的属性，构建基决策树们。然后做法和bagging相同，用简单平均（连续）或投票法（离散）得到最终学习器。
+# >极端随机森林即k=1
+
+
 # `.RandomForestClassifier(n_estimators=’warn’, criterion=’gini’, max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=’auto’, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=False, n_jobs=None, random_state=None, verbose=0, warm_start=False, class_weight=None)`
 # `.RandomForestRegressor(n_estimators=’warn’, criterion=’mse’, max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=’auto’, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=False, n_jobs=None, random_state=None, verbose=0, warm_start=False)`
 from sklearn.ensemble import RandomForestClassifier
@@ -232,7 +251,9 @@ plt.show()
   
   
 # 3 <Stacking>
-# 先训练初级学习器，然后用预测值来训练次级学习器
+# >先训练初级学习器，然后用预测值来训练次级学习器
+# >训练几个初级学习器，然后用他们的预测结果来训练次级（元）学习器。
+
 from sklearn import datasets
 iris = datasets.load_iris()
 X, y = iris.data[:, 1:3], iris.target
