94448e54b0de0c4ab48af6bce6022260fe1ed298
lihao <41355487+lihao13758362949@users.noreply.github.com>
2021-02-23 22:52:50 +0800

    Create Dimensionality_Reduction.py

---
+++ baselib/Dimensionality_Reduction.py
@@ -0,0 +1,28 @@
+'''
+降维算法
+Dimensionality_Reduction.py
+
+降维的作用：
+1. 使得数据集更易使用
+2. 降低计算开销
+3. 去除噪声
+4. 使得结果易懂
+'''
+
+# 1 <多维缩放>(Multiple Dimensional scaling,MDS)
+
+# 2 <主成分分析>(Principal Component Analysis,PCA)
+'''
+伪代码：
+1. 中心化
+2. 计算协方差矩阵
+3. 计算协方差矩阵的特征值和特征向量
+4. 降序特征值
+5. 保留前N个特征向量
+6. 转换到这N个特征向量构建的空间中
+'''
+
+
+# 3 <因子分析>(Factor Analysis,FA)
+
+# 4 <独立成分分析>(Independent Component Analysis,ICA)
