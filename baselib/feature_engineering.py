db04f0c2251a448676dedfa42527e2de02f2aea4
李豪 <41355487+lihao13758362949@users.noreply.github.com>
2021-07-08 22:56:57 +0800

    Add files via upload

--- baselib/feature_engineering.py
+++ baselib/feature_engineering.py
@@ -1,26 +1,18 @@
-'''
-<特征工程> 
-输入：train_pro.csv,test_pro.csv
+"""
+<特征工程>
+输入：all_data
 输出：train_final.csv,test_final.csv或 train_for_tree.csv,test_for_tree.csv,train_for_lr.csv,...
 
 >特征决定你的上限，模型只不过在无限逼近这个值罢了。
->有人总结 Kaggle 比赛是 “Feature 为主，调参和 Ensemble 为辅”，我觉得很有道理。Feature Engineering 能做到什么程度，取决于对数据领域的了解程度。
->比如在数据包含大量文本的比赛中，常用的 NLP 特征就是必须的。怎么构造有用的 Feature，是一个不断学习和提高的过程。
->特征工程与EDA联系紧密，可以说是EDA具体的操作吧。因为数据分析本身就是“假设”-“分析”-“验证”的过程，这个验证的过程一般是指构建特征并进行本地CV验证。
->特征工程本质做的工作是，将数据字段转换成适合模型学习的形式，降低模型的学习难度。
+>有人总结 Kaggle 比赛是 “Feature 为主，调参和 Ensemble 为辅”
 >之所以构造不同的数据集是因为，不同模型对数据集的要求不同
 
->我在做特征工程的时候主要依靠两条线索。一条从问题本身出发，比如对于点击率预估问题，考虑用户会怎么想，用户会关心什么，同时也考虑商品适合哪些用户，购买这些商品的人有哪些共同点。
->另一条从特征类型出发，比如考虑做哪些特征交叉；哪些特征在分布上非常诡异，需要做一些预处理；哪些特征是多值类别特征，需要做特殊操作；哪些特征的量纲一致，可以做比较以及求和。
->在做特征的时候，尽量做得细致全面，不要在比赛初期考虑哪些特征会对模型产生副作用就放弃采用。因为只要严格保证自己的特征在训练集、验证集和测试集是一致的（特征的含义严格一致，同时特征的取值分布也基本一致），
->理论上这些特征就都不会对模型产生副作用（对于极个别无法保证一致的特征，可以在公榜上实验。换榜时，也要格外注意这些特征）。
 > 自动特征工程FeatureTools
-'''
+"""
 
-# 1 <连接数据>
-train_pro = pd.read_csv('train_pro.csv')
-test_pro = pd.read_csv('test_pro.csv')
-all_data = pd.concat([train_pro.assign(is_train=1), test_pro.assign(is_train=0)],ignore_index=True, sort=False) #把训练集和测试集一起处理可以减少代码量
+import pandas as pd
+import gc
+
  
 # 2 <加变量> <特征提取>（Feature Extraction）
 ## 2.1 <简化>
@@ -43,9 +35,25 @@ train["OverallQual-Sq"] = np.sqrt(train["OverallQual"])
 
 ## 2.5 <其它>
 # >如可以把一个变量是否为0作为特征
+def geo_feature():
+    # 地理位置相关特征
+    # 按经纬度进行网格划分，用word2vec及逆行训练得到编码向量
+    # 然后区域统计特征
+    pass
 
-## 2.6 <统计特征>
-# >all_data.groupby.agg()：生成统计特征（count，max，min，sum，mean，极差），用法如下
+def level1_feature():
+    # 一阶统计特征
+    # count，max，min，sum，mean，极差，偏度，峰度
+    pass
+
+
+def level2_feature():
+    # 二阶统计特征（时间序列常用）
+    # 差分的均值、方差
+    pass
+
+
+# all_data.groupby.agg()
 data['count'] = 1
 tmp = data[data['goods_has_discount']==1].groupby(['customer_id'])['count'].agg({'goods_has_discount_counts':'count'}).reset_index()
 customer_all = customer_all.merge(tmp,on=['customer_id'],how='left')
@@ -78,13 +86,26 @@ brand_fe = pd.DataFrame(all_info).T.reset_index().rename(columns={"index": "bran
 data = data.merge(brand_fe, how='left', on='brand')
 
 # >all_data.groupby.last/min/max....
+def create_time_feat(df, time_feat):
 
-## 2.7 <时间特征>
-# >可以利用时间特征来划分数据集，滑动窗口
-train_history = train[(train['order_pay_date'].astype(str)<='2013-07-03')]
-train_label = train[train['order_pay_date'].astype(str)>='2013-07-04']
+    # 时间特征构造
+    # 可以利用时间特征来划分数据集，滑动窗口
+    # train_history = train[(train['order_pay_date'].astype(str)<='2013-07-03')]
+    # train_label = train[train['order_pay_date'].astype(str)>='2013-07-04']
+    df[time_feat] = pd.to_datetime(df[time_feat])
+    df['date'] = df[time_feat].dt.date
+    df['day'] = df[time_feat].dt.day
+    df['hour'] = df[time_feat].dt.hour
 
 
+def add_prefix(df, exclude_columns, prefix):
+    # 给特征增加前缀
+    if isinstance(exclude_columns, str):
+        exclude_columns = [exclude_columns]
+
+    column_names = [col for col in df.columns if col not in exclude_columns]
+    df.rename(columns=dict(zip(column_names, [prefix + name for name in column_names])), inplace=True)
+    return df
 # 2 <减变量><特征选择>（Feature Selection）
 ## 2.1 <降维算法> 
 
@@ -148,35 +169,23 @@ print(high_range)
 对于字符型的特征，要在编码后转换数据类型pandas.DataFrame.astype
 '''
 from sklearn.preprocessing import OrdinalEncoder,OneHotEncoder
-## 5.1 <手动编码>
-train = train.replace({"MSSubClass" : {20 : "SC20", 30 : "SC30", 40 : "SC40", 45 : "SC45", 
-                                       50 : "SC50", 60 : "SC60", 70 : "SC70", 75 : "SC75", 
-                                       80 : "SC80", 85 : "SC85", 90 : "SC90", 120 : "SC120", 
-                                       150 : "SC150", 160 : "SC160", 180 : "SC180", 190 : "SC190"},
-                       "MoSold" : {1 : "Jan", 2 : "Feb", 3 : "Mar", 4 : "Apr", 5 : "May", 6 : "Jun",
-                                   7 : "Jul", 8 : "Aug", 9 : "Sep", 10 : "Oct", 11 : "Nov", 12 : "Dec"}
-                      })
 
+def encode_by_hand(df, cols, map):
+    # 手动编码
+    # cols是要替换的列的list
 
-title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5} # 手动编码
-dataset['Title'].map(title_mapping)
-col_dicts = {}
-cols = ['checking_balance','credit_history', 'purpose', 'savings_balance', 'employment_length', 'personal_status', 
-        'other_debtors','property','installment_plan','housing','job','telephone','foreign_worker']
-col_dicts = {'checking_balance': {'1 - 200 DM': 2,
-  '< 0 DM': 1,
-  '> 200 DM': 3,
-  'unknown': 0},
- 'credit_history': {'critical': 0,
-  'delayed': 2,
-  'fully repaid': 3,
-  'fully repaid this bank': 4,
-  'repaid': 1}}
-for col in cols:
-    credit[col] = credit[col].map(col_dicts[col])
-    
-# credit.head(9)
-
+    # maps = {'checking_balance': {'1 - 200 DM': 2,
+    #   '< 0 DM': 1,
+    #   '> 200 DM': 3,
+    #   'unknown': 0},
+    #  'credit_history': {'critical': 0,
+    #   'delayed': 2,
+    #   'fully repaid': 3,
+    #   'fully repaid this bank': 4,
+    #   'repaid': 1}}
+    for col in cols:
+        df[col] = df[col].map(map[col])
+    return df
 
 ## 5.2 <序号编码>Ordinary Encoding
 OrdinalEncoder(categories=’auto’, dtype=<class ‘numpy.float64’>)
@@ -206,12 +215,14 @@ data = pd.get_dummies(data, columns=['model', 'brand', 'bodyType', 'fuelType',
 # Best-KS 分桶（类似利用基尼指数进行二分类）；
 # 卡方分桶；
 连续特征离散化pandas.cut，然后通过pandas.Series.value_counts观察切分点，再将对应bins或者说区间的连续值通过pandas.DataFrame.replace或pandas.DataFrame.loc到离散点0,1,2,…后再进行编码。
-                             
-# 9 <最终输出>
-print(all_data.shape)
-all_data.columns
-all_data[all_data['is_train']==1].to_csv('train_fianl.csv', index=0)
-all_data[all_data['is_train']==0].to_csv('test_final.csv', index=0)
+
+
+def data_output(label, all_data):
+    # 最终输出>
+    print(all_data.shape)
+    print(f'features: {list(all_data.columns)}')
+    all_data[all_data['is_train']==1].drop('is_train', axis=1).to_csv('train_final.csv', index=0)
+    all_data[all_data['is_train']==0].drop([label,'is_train'], axis=1).to_csv('test_final.csv', index=0)
                              
                              
                              
